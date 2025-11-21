052824c2ec5f7db6db3a81edb7f43aa77f139267
Alex Yang <2892565+aycyang@users.noreply.github.com>
2025-11-22 02:52:00 +0800

    Update scripts/record_progress.sh for new test location

--- scripts/record_progress.sh
+++ scripts/record_progress.sh
@@ -6,6 +6,6 @@ a() {
   git log -n 1 --pretty=format:%at
 }
 b() {
-  npx tsx --test-reporter=tap tests/all.ts | grep pass | cut -d' ' -f3
+  npx tsx --test --test-reporter=tap | grep pass | cut -d' ' -f3
 }
 echo `a`,`b`
