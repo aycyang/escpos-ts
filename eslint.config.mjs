dff2ed9c4df3fb9ef57d419202ef635d14fdb17a
Alex Yang <2892565+aycyang@users.noreply.github.com>
2025-11-21 23:44:50 +0800

    Separate tsconfig just for eslint
    tsc should not run on unit test files, lest we distribute our unit
    tests to clients, so the main tsconfig should exclude unit test files.
    However, this causes eslint to complain when it goes to lint the unit
    test files:
    
        <unit test file> was not found by the project service. Consider
        either including it in the tsconfig.json or including it in
        allowDefaultProject
    
    To fix this, a separate tsconfig just for eslint is added that includes
    the unit test files, but isn't used by tsc.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,19 +2,13 @@ import eslint from '@eslint/js'
 import { defineConfig } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 
-/*
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
-*/
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   {
     languageOptions: {
       parserOptions: {
-        projectService: true,
+        project: ['./tsconfig.eslint.json'],
       },
     },
   },
